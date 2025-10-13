971896f09b33a6604169b755422d620e428dd55d
maxconway <maxlconway@gmail.com>
2025-10-13 11:49:56 -0600

    add minimal docker setup

---
+++ Docker/docker_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -f "$@" -t h12_cameracalib:latest .
---
+++ Docker/docker_run_desktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Allows the container to connect to your display for GUIs
+xhost +local:root
+
+# Run the container with maximum host resources
+docker run --rm -it \
+  --name h12_cameracalib-container \
+  -v /home/max/vp_ws/src:/ros2_ws/src \
+  --gpus all \
+  --network host \
+  --ipc=host \
+  --pid=host \
+  --privileged \
+  --device /dev/bus/usb:/dev/bus/usb \
+  -v /dev:/dev \
+  -e DISPLAY="$DISPLAY" \
+  -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/timezone:/etc/timezone:ro \
+  h12_cameracalib:latest \
+  "$@"
